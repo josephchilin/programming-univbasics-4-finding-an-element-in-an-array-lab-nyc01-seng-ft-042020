0424646ce29019d072845351ed2a7c9eb25b511d
Jo L <joseph@josephchilin.com>
2020-04-13 02:42:54 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,10 @@ def find_element_index(array, value_to_find)
 
 counter = 0
   while counter < array.length do
-
+    if array[counter] == value_to_find
+      found_value = counter
+    end  
     counter +=1
   end
+  found_value
 end
